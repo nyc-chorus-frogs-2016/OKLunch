cfeb5df2983f8141ef6d0641200b7a16cc632f57
K. B. DiAngelo <email": null>
2016-02-18 17:41:08 -0500

    make some progress on the welcome/about page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,14 @@
 class RestaurantsController < ApplicationController
 
+  #### THIS IS JUST SO I CAN GO TO THE CUSTOM ABOUT ####
+  #### INSTEAD OF THE LOGIN PAGE ####
+  #### KB : Also, delete these comments ####
+  skip_before_action :ensure_current_user
+
   def index
+    unless current_user
+      redirect_to about_path layout: false
+    end
     @restaurants = Restaurant.all
   end
 end
