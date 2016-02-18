065e1e0cae5ea44026b3241a505b6b0fb27b1ea9
Scott Chou <iscott.chou@gmail.com>
2016-02-18 15:50:14 -0500

    Add OAth gem, and edit user migration/model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,28 @@ class User < ActiveRecord::Base
     :join_table => "match_users",
     :foreign_key => "creator_id",
     :association_foreign_key => "target_id")
+  validates :name, presence: true
+  validates :uid, presence: true, uniqueness: true
+  validates :uid, uniqueness: { scope: :provider }
 
   def find_common_interests(usertwo)
 
+  end
+
+
+
+
+  def self.sign_in_from_omniauth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+  end
+
+  def self.create_user_from_omniauth(auth)
+    create(
+      provider: auth['provider'],
+      uid: auth['uid'],
+      name: auth['info']['name'],
+      password: "1234"
+      )
   end
 
 end
