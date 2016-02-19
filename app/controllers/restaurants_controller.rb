f5048fb5b8ffd66492aa2521467d0d24cd45a457
Edward <email": null>
2016-02-19 15:57:34 -0500

    Update restaurant controller and model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,7 +15,11 @@ class RestaurantsController < ApplicationController
   def filtered
     search_filter = params[:searchfield]
     target = params[:search]
-     @filtered_restaurants = Restaurant.where(search_filter.to_sym => target)
+    @filtered_restaurants = Restaurant.where(search_filter.to_sym => target)
+  end
+
+  def next_unswiped
+    render :json => Restaurant.next(current_user).to_json
   end
 
 end
