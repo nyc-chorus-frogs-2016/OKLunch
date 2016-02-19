0522b01a6b958e94ec50c413921221130d1100b0
Edward <email": null>
2016-02-19 16:03:00 -0500

    Add migration and model for user-restaurant swipes

--- app/assets/javascripts/restaurant_index/view.js
+++ app/assets/javascripts/restaurant_index/view.js
@@ -1,10 +1,24 @@
-function View() {
-
-};
+function View(){};
 
 View.prototype.drawRestaurant = function(thisRestaurant){
+  var html = '<h1>THIS IS THE RESTAURANT DETAILS</h1>'
+
+  $('div#active-restaurant').append(html)
+  debugger;
 }
 
 View.prototype.listenForDrop = function(){
 
 }
+//take this driver code out
+$(document).ready(function(){
+  view = new View;
+  controller = new Controller(view);
+  view.controller = controller;
+
+  $('a#testing-button').on("click", function(event){
+    firstRestaurant = controller.getNextUnswipedRestaurant;
+    view.drawRestaurant(firstRestaurant);
+  });
+
+});
---
+++ app/models/swipe.rb
@@ -0,0 +1,4 @@
+class Swipe < ActiveRecord::Base
+  belongs_to :restaurant
+  belongs_to :user
+end
