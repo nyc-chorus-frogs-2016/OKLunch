c6906e72effff085edb38e9e7336237c5bf1b773
Scott Chou <iscott.chou@gmail.com>
2016-02-20 15:40:11 -0500

    Add Pro Pic for Omni-Auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-           scope: 'public_profile', display: 'page', image_size: 'square'
+           scope: 'public_profile', display: 'page', image_size: 'normal'
 end
 # 815593005235923
 # 73caffddb50e318596a66b002d63ac5e
