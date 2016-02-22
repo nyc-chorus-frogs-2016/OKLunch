7aefa31559516f80c62c2e685cc7687d7a358ff8
Edward <email": null>
2016-02-22 17:23:20 -0500

    filter swiping complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,7 @@ class Restaurant < ActiveRecord::Base
   has_many :swipes
 
   def self.next(user)
-	self.all.to_a.reject {|restaurant| restaurant.match_restaurants.where(user_id: user.id).any?}.sample
+   self.all.to_a.reject {|restaurant| restaurant.match_restaurants.where(user_id: user.id).any?}.sample
   end
+
 end
