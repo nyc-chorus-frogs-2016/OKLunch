a9e59310e3a1f88a58c1e43ac383b1d0ed0ec396
Edward & K. B. DiAngelo & Scott Chou <pair+edwardgemson.kb-diangelo.scottychou@devbootcamp.com>
2016-02-22 09:48:19 -0500

    Swiping a restaurant creates a matchrestaurant link

--- app/assets/javascripts/restaurant_index/controller.js
+++ app/assets/javascripts/restaurant_index/controller.js
@@ -4,22 +4,17 @@ function Controller(view){
 
 
 Controller.prototype.getNextUnswipedRestaurant = function(){
-  // Make an ajax call to the custom route that returns
-  // the next unswiped restaurant for that user
-
   var options = new Object();
   options.url = '/restaurants/next_unswiped'
   options.method ='GET'
   options.dataType = 'json'
   $.ajax(options).done(function(response){
-    debugger;
     view.drawRestaurant(response);
     view.makeDraggable();
   }).fail(function(response){
     console.log(response);
   });
-
-  };
+};
 
 Controller.prototype.submitDrop = function(args){
   var dataObj = {match: args.match, restaurant_id: args.restaurantID, is_permanent: args.isPermanent}
