9bb3d71f74f7700e09f58d54351cad8ad4867027
Edward & K. B. DiAngelo & Scott Chou <pair+edwardgemson.kb-diangelo.scottychou@devbootcamp.com>
2016-02-22 11:20:29 -0500

    Validate the uniqueness of a matchuser, validate that the two users are different

--- app/models/match_user.rb
+++ app/models/match_user.rb
@@ -1,4 +1,16 @@
 class MatchUser < ActiveRecord::Base
   belongs_to :creator, foreign_key: :creator_id, class_name: "User"
   belongs_to :target, foreign_key: :target_id, class_name: "User"
+
+  validates :creator_id, uniqueness: {scope: :target_id}
+
+  validate :different_users, on: :create
+
+  def different_users
+    if self.creator_id == self.target_id
+      errors.add(:creator, "creator can't be target")
+    end
+  end
+
+
 end
