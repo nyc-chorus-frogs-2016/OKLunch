11118e73180e710a656cade7438e80d59ef03673
Edward <email": null>
2016-02-23 15:01:39 -0500

    add ajax for all positive matches

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   $('.user-swipe-yes').on('submit', function(event){
     event.preventDefault();
+    userSwipeeId = event.currentTarget[2].value
     var requestOptions = {
       url: $(event.target).attr('action'),
       method: 'POST',
@@ -9,13 +10,12 @@ $(document).ready(function(){
       dataType: 'HTML'
     }
     $.ajax(requestOptions).done(function(response){
-      debugger
       console.log(response)
-      $('#yes-no-buttons').replaceWith(response)
+      $('[data-id=' + userSwipeeId + ']').replaceWith(response)
     }).fail(function(response){
       console.log(response)
     });
-  })
+  });
   //   $('input.submit-no-swipe').on('click', function(event){
   //   event.preventDefault();
   //   debugger
