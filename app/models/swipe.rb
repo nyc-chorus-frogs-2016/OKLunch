94277a37f1773af5074421d7814d75d5ef5bb545
Edward & K. B. DiAngelo & Scott Chou <pair+edwardgemson.kb-diangelo.scottychou@devbootcamp.com>
2016-02-23 16:55:32 -0500

    minor changes

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -7,12 +7,6 @@ class Swipe < ActiveRecord::Base
 
   validates :swiper_id, uniqueness: {scope: [:swipee_id, :restaurant_id]}
 
-  def search_for_match(swipe_creator)
-  	current_swipe = Swipe.find_by(swiper_id: swipe_creator.id)
-  	match_swipe = Swipe.where(swiper: current_swipe.swipee, swipee: current_swipe.swiper)
-  	match_swipe.exists?
-  end
-
   def parallel_positive_swipe_exists
     Swipe.where(direction: true).where(swipee_id: self.swiper_id).where(swiper_id: self.swipee_id).count > 0
   end
