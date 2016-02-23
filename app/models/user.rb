025927574acbe1ff30c7c725ee075644cf316218
kb-diangelo <kdiangelo@gmail.com>
2016-02-23 02:57:22 +0000

    do a little work on user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+
   has_secure_password
+
   has_many :interest_users
   has_many :interests, through: :interest_users
+
+  has_many :match_users, foreign_key: :creator_id
+  has_many :match_users, foreign_key: :target_id
+
   has_and_belongs_to_many(:users,
     :join_table => "match_users",
     :foreign_key => "creator_id",
-    :association_foeign_key => "target_id")
-  has_many :restaurants, through: :match_restaurants
+    :association_foreign_key => "target_id")
 
-  validates :name, presence: true
+  has_many :restaurants, through: :match_restaurants
 
   has_many :swipes_as_swipee, foreign_key: :swipee_id, class_name: "Swipe"
   has_many :swipes_as_swiper, foreign_key: :swiper_id, class_name: "Swipe"
